3579bf19cdc49bf074117ebab60842fa8043d9b3
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2022-08-18 11:17:50 +0100

    Refactoring 2

--- app/src/main/java/com/example/countriesoftheworld/view/fragments/AllCountriesFragment.kt
+++ app/src/main/java/com/example/countriesoftheworld/view/fragments/AllCountriesFragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.countriesoftheworld.databinding.FragmentNotificationsBinding
+import com.example.countriesoftheworld.databinding.FragmentAllCountriesBinding
 
-class NotificationsFragment : Fragment() {
+class AllCountriesFragment : Fragment() {
 
-    private var mBinding: FragmentNotificationsBinding? = null
+    private var mBinding: FragmentAllCountriesBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        mBinding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        mBinding = FragmentAllCountriesBinding.inflate(inflater, container, false)
         return mBinding!!.root
     }
 
---
+++ app/src/main/java/com/example/countriesoftheworld/view/fragments/FavouriteCountriesFragment.kt
@@ -0,0 +1,27 @@
+package com.example.countriesoftheworld.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.countriesoftheworld.databinding.FragmentFavouriteCountriesBinding
+
+class FavouriteCountriesFragment : Fragment() {
+
+    private var mBinding: FragmentFavouriteCountriesBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mBinding = FragmentFavouriteCountriesBinding.inflate(inflater, container, false)
+        return mBinding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
+    }
+}
